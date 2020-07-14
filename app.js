d8976334abdc60c1c59e22a44564b6a08f83e3bd
Connor Twele <connortwele@gmail.com>
2020-07-13 21:34:10 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var family = {
+    'last name': 'Twele',
+    'size': 3,
+    'members':['Connor','Susan','Winnie']
+
+}
+var random = {
+    'number': 8,
+    'string':'example',
+    'array': [1,2,3]
+}
+
+var randomTwo = {
+    'numberTwo': 2,
+    'stringTwo': 'elpmaxe',
+    'arrayTwo': [3,2,1]
+}
+
+console.log(family.members);
+
+console.log(random['string']);
+
+console.log(randomTwo.numberTwo);
+
+
+var dude = [6,9,4,2,0]
+function nice(arg1) {
+    arg1[i]
+    for (var i = 0; i < nice.length; i++){
+        console.log(arg1);
+    }
+};
+nice(dude)
+
+
+
